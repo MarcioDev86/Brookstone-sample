014dd40e9daf4fa31d385fee910624cd8bc4ce0c
Marcio <mecastillo00@gmail.com>
2019-09-23 21:27:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        style={{
+          height: "100vh",
+          border: "none"
+        }}
+        title="Brookstone video"
+        width="100%"
+        src="https://www.youtube.com/embed/QB-OKlkOQmQ?autoplay=1&mute=1&enablejsapi=1"
+      ></iframe>
     </div>
   );
 }
 
 export default App;
+
+/**
+ *
+ *
+ */
